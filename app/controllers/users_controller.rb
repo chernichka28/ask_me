78bef4c8a7f979ebe5d3d94f733f7bd46a29428b
dmitry <dmitryadygezalov@gmail.com>
2022-12-12 16:45:07 +0300

    add some validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    @user = User.create(user_params)
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
 
-    session[:user_id] = @user.id
+    @user = User.new(user_params)
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались и вошли!'
+    if @user.save
+      session[:user_id] = @user.id
+
+      redirect_to root_path, notice: "Вы успешно зарегистрировались и вошли!"
+    else
+      flash.now[:alert] = "Вы неправильно заполнили поля формы регистрации!"
+
+      render :new
+    end
+
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w.-]+@[\w_\-\.+]+\.\w+\z/}
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[A-Za-z_\d]+\z/}
 end
