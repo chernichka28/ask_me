aef9fe009a94219e2c08839cd9aed08c4fceb42b
dmitry <dmitryadygezalov@gmail.com>
2022-12-12 13:46:32 +0300

    add user and registration form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.create(user_params)
+
+    session[:user_id] = @user.id
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались и вошли!'
+  end
+
+  def new
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root to: "questions#index"
+
   resources :questions do
     member do
       put "hide"
     end
   end
+
+  resource :session, only: %i[new create destroy]
+
+  resources :users, only: %i[new create]
 end
