5d425ab3818441d3428f60f781a76287214ab99d
dmitry <dmitryadygezalov@gmail.com>
2022-12-12 22:23:59 +0300

    fixed nickname validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  before_validation :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w.-]+@[\w_\-\.+]+\.\w+\z/}
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[A-Za-z_\d]+\z/}
