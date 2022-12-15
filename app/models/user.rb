07cd575cc3a8ce4914f835acc6ed1d569b6079c4
dmitry <dmitryadygezalov@gmail.com>
2022-12-15 11:51:57 +0300

    edit email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,18 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  before_validation :downcase_nickname
+  before_validation :downcase_nickname, :downcase_email
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w\.-]+@[A-Za-z\d\-\.+]+\.[a-z]+\z/}
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[A-Za-z_\d]+\z/}
 
+  private
+
   def downcase_nickname
     nickname.downcase!
+  end
+
+  def downcase_email
+    email.downcase!
   end
 end
